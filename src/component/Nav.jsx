180c8f75e0399a78e5c1048d7538f3d749dcd498
Abdianggi <59114554+Abdianggi@users.noreply.github.com>
2025-01-09 16:35:00 +0800

    change tab mobile responsive

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -25,15 +25,15 @@ export default function Nav() {
     };
 
     return (
-        <div className="flex flex-wrap justify-between items-center py-6 px-6 lg:px-20 xl:px-52 md:pb-6 md:pt-10 bg-white shadow-gray-300 shadow-md mb-10">
+        <div className="flex flex-wrap justify-between items-center py-6 px-4 lg:px-20 xl:px-52 md:pb-6 md:pt-10 bg-white shadow-gray-300 shadow-md mb-10">
             {!isMinMd ? (
                 <>
                     <div className="items-center text-xs sm:text-md mb-4 sm:mb-0">
-                        <img src={hybridIcon} alt="Hybrid Icon" className="mr-3 w-2/3 sm:w-full" />
-                        <div className="text-[#0B8BEE] ml-2">Affiliator System</div>
+                        <img src={hybridIcon} alt="Hybrid Icon" className="mr-[-1.5rem] sm:mr-3 w-2/3 sm:w-full" />
+                        <p className="text-[#0B8BEE] ml-1 sm:ml-2">Affiliator System</p>
                     </div>
 
-                    <div className="flex items-center bg-[#F8F8F8] p-3 rounded-sm mb-4 sm:mb-0">
+                    <div className="sm:flex items-center bg-[#F8F8F8] p-3 rounded-sm mb-4 sm:mb-0 hidden">
                         <div className="bg-slate-900 pt-2 pb-1 pl-5 pr-5 text-slate-50 text-center align-middle mr-3 hidden md:block">QR <br/> Code</div>
                         <div>
                             <div className="text-xs sm:text-sm mb-2">QR Affiliate Page</div>
@@ -67,19 +67,19 @@ export default function Nav() {
                     </div>
                 </div>
             )}
-
+            
             {/* Profile Section */}
-            <div className="flex items-start cursor-pointer" onClick={toggleProfileCard}>
+            <div className="flex items-start cursor-pointer mb-8" onClick={toggleProfileCard}>
                 <div className="block content-center pt-3">
                     <div className="text-[#004174] text-right text-xs sm:text-sm">Putu Abdi</div>
                     <div className="flex">
                         <div className="text-xs text-[#808080] font-light mr-1">Affiliator ID :</div>
-                        <div className="pl-2 pr-2 pt-[1px] pb-[2px] bg-[#004174] text-[0.5rem] sm:text-xs text-center text-white rounded-sm">AFF001</div>
+                        <div className="px-1 pt-[1px] pb-[2px] bg-[#004174] text-[0.5rem] sm:text-xs text-center text-white rounded-sm">AFF001</div>
                     </div>
                 </div>
                 <img 
                     src="https://media.licdn.com/dms/image/v2/D5603AQGaAOkGotaE3g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1669815154431?e=1740009600&v=beta&t=NBiadf-Ba0OXBtE-sV8-AoDl8TFmri0w4ZsCM1GyBOE" 
-                    className="w-10 h-10 rounded-full ml-3 mt-2 sm:mt-4" 
+                    className="w-10 h-10 rounded-full ml-1 mt-2 sm:ml-3 sm:mt-4" 
                     // alt="Profile" 
                 />
             </div>
@@ -130,7 +130,7 @@ export default function Nav() {
 
             {/* Profile Card */}
             {showProfileCard && (
-                <div className="absolute top-[7rem] sm:right-[13rem] bg-white shadow-[0_0_20px_rgba(0,0,0,0.2)] w-64 rounded-md z-10">
+                <div className="absolute top-[6rem] md:top-28 right-3 sm:right-[13rem] bg-white shadow-[0_0_20px_rgba(0,0,0,0.2)] w-64 rounded-md z-10">
                     {/* Bagian Header */}
                     <div className="border-b py-2 px-3 rounded-t-md flex justify-between">
                         <div className="text-sm font-normal">Account</div>
--- src/component/Tabs.jsx
+++ src/component/Tabs.jsx
@@ -1,11 +1,12 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Tabs({disabled}) {
   const tabs = [
-    { id: "dashboard", label: "Dashboard", path: "/dashboard" , icon : <i class="fa-solid fa-users text-xs"></i>},
-    { id: "booking", label: "Booking", path: "/booking" , icon : <i class="fa-solid fa-calendar-days text-xs"></i>},
-    { id: "withdrawal", label: "Withdrawal", path: "/withdrawal" , icon : <i class="fa-solid fa-money-bill text-xs"></i>},
-    { id: "affiliator", label: "Profile", path: "/affiliator-profile" , icon : <i class="fa-regular fa-user text-xs"></i>},
+    { id: "dashboard", label: "Dashboard", path: "/dashboard" , icon : <i class="fa-solid fa-users text-xs px-1"></i>},
+    { id: "booking", label: "Booking", path: "/booking" , icon : <i class="fa-solid fa-calendar-days text-xs px-1"></i>},
+    { id: "withdrawal", label: "Withdrawal", path: "/withdrawal" , icon : <i class="fa-solid fa-money-bill text-xs px-1"></i>},
+    { id: "affiliator", label: "Profile", path: "/affiliator-profile" , icon : <i class="fa-regular fa-user text-xs px-1"></i>},
   ];
 
   const handleClick = (e) => {
@@ -14,34 +15,91 @@ export default function Tabs({disabled}) {
     }
   };
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const toggleModal = () => {
+      if (isModalOpen) {
+          setIsClosing(true);
+          setTimeout(() => {
+              setIsModalOpen(false);
+              setIsClosing(false);
+          }, 700); // Waktu untuk animasi slide-down
+      } else {
+          setIsModalOpen(true);
+      }
+  };
+
   return (
-    <div className="flex space-x-1 border-gray-300 px-12 md:px-32 xl:px-72">
-      {tabs.map((tab) => (
-        <NavLink
-          key={tab.id}
-          to={tab.path}
-          onClick={handleClick}
-          className={({ isActive }) =>
-            `flex py-1 sm:py-2 px-3 sm:px-4 text-sm font-normal rounded-t-lg${disabled ? `cursor-not-allowed opacity-50` : ``} ${
-              isActive
-                ? "border-blue-500 text-white bg-[#1B8BE3]"
-                : "border-transparent text-black bg-white hover:text-[#1B8BE3] hover:border-gray-300"
-            }`
-          }
-        >
-            {tab.icon}
-            <div className="hidden text-xs sm:inline-block sm:text-sm sm:ml-2">
-              {tab.label}
-            </div>
-{/*             
-            <div className="hidden sm:block mr-1">
+    <div className="sticky top-[92%] bg-gradient-to-t from-[#ffffff] my-[-2.5rem] flex items-center justify-center  sm:my-0 sm:from-transparent sm:bg-transparent sm:static pb-3 sm:p-0 sm:block">
+      <div className="sticky top-[92%] z-10 flex bg-white max-w-content rounded-full p-3 mx-5 shadow-[0px_12px_40px_0px_rgba(0,0,0,0.25)] sm:static sm:my-0 sm:shadow-none sm:rounded-none sm:bg-transparent sm:space-x-1 sm:border-gray-300 sm:py-0 sm:px-12 md:px-32 xl:px-72">
+        {tabs.map((tab) => (
+          <NavLink
+            key={tab.id}
+            to={tab.path}
+            onClick={handleClick}
+            className={({ isActive }) =>
+              `flex p-2 rounded-full mx-2 sm:mx-0 sm:py-2 sm:px-4 text-sm font-normal sm:rounded-none sm:rounded-t-lg ${disabled ? `cursor-not-allowed opacity-50` : ``} ${
+                isActive
+                  ? "border-blue-500 text-white bg-[#1B8BE3]"
+                  : "border-transparent text-black bg-white hover:text-[#1B8BE3] hover:border-gray-300"
+              }`
+            }
+          >
               {tab.icon}
+              <div className="hidden text-xs sm:inline-block sm:text-sm sm:ml-2">
+                {tab.label}
+              </div>
+          </NavLink>
+        ))}
+      </div>
+      <div onClick={toggleModal} className="sticky top-[92%] z-10 left-60 bg-white w-14 h-14 rounded-full shadow-[0_4px_8px_rgba(0,0,0,0.1)] flex items-center justify-center sm:hidden cursor-pointer">
+        <i className="fas fa-qrcode"></i>
+      </div>
+      
+      {/* Modal */}
+      {isModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-10 z-50">
+            {/* Modal Container */}
+            <div
+                className={`bg-transparent rounded-lg grid grid-cols-[1fr_auto] ${
+                    isClosing ? "animate-slide-down" : "animate-slide-up"
+                }`}
+            >   
+                <div className="bg-white px-5 pb-5 pt-2 rounded-md">
+                    <div className="flex justify-center">
+                        <div className="text-md text-center self-center font-semibold mb-4 rounded-md w-28 border-[1px]">
+                                Affiliate Page
+                        </div>
+                    </div>
+                    <img
+                        src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&size=500x500"
+                        alt="QR Code"
+                        className="mx-auto mb-4 w-72 h-72"
+                    />
+                    <div className="flex justify-between">
+                        <button
+                            className="bg-[#F9F9F9] text-[#504F4F] text-xs font-medium px-2 py-1 rounded-sm"
+                        >
+                            Download
+                        </button>
+                        <button
+                            className="bg-[#0B8BEE] text-white text-xs font-medium px-2 py-1 rounded-sm"
+                        >
+                            Print
+                        </button>
+                    </div>
+                </div>
+                <button
+                    className="bg-white text-black text-xl self-start ml-2 font-medium px-2 py-1 rounded-md"
+                    onClick={toggleModal}
+                >
+                    &#x2715;
+                </button>
             </div>
-            <div className="text-[0.7rem] sm:text-sm">
-              {tab.label}
-            </div> */}
-        </NavLink>
-      ))}
+
+          </div>
+      )}
     </div>
   );
 }
